7d6c0361696c41e922deac1ea75374382581f9cb
macosx1005 <vladut.georgescu@yahoo.com>
2021-04-26 16:41:23 +0300

    commit

---
+++ resources/views/todo/edit.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+@section('content')
+    <div class="w-110 h-100 d-flex justify-content-center align-items-center">
+        <div class="text-center" style="width: 40%">
+        
+            <h1 class="display-3 text-black">
+                  Edit your Todo called: {{$todo->title}}
+            </h1>
+           
+            <form action="{{route('todo.update', $todo->id)}}" method="POST">
+                @csrf 
+                @method('PUT')
+                <div class="input-group mb-3 w-100">
+                    <input type="text" class="form-control form-control-lg" name='title' value="{{$todo->title}}" aria-label="Recipient's username" aria-describedby="button-addon2">
+                    <div>
+                          <button class="btn btn-success" type="submit" id="button-addon2">Save</button>
+                    </div>
+
+                </div>
+            
+            </form>
+         
+        </div>
+
+     </div>
+
+    
+@endsection
+
